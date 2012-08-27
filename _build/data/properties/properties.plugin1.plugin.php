e499ba19428a16bc3b7337f70925643e875ba9eb
bobray <bobray@softville.com>
2012-08-27 18:45:39 -0500

    added empty properties file for plugin1

---
+++ _build/data/properties/properties.plugin1.plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Properties file for Snippet1 snippet
+ *
+ * Copyright 2012 by Bob Ray <http://bobsguides.com>
+ * Created on 08-25-2012
+ *
+ * @package example
+ * @subpackage build
+ */
+
+
+
+
+
+
