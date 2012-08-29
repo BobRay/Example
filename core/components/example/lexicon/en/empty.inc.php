b93a4f2882d544db25db81e7a47cab60beaf1bee
bobray <bobray@softville.com>
2012-08-29 00:47:02 -0500

    Added bogus lexicon file, empty lexicon file, and unused lexicon string

---
+++ core/components/example/lexicon/en/empty.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Bob Ray
+ * Date: 8/28/12
+ * Time: 11:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/* This language file has no language strings */
---
+++ core/components/example/lexicon/en/forms.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Bob Ray
+ * Date: 8/28/12
+ * Time: 10:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+$_lang['string10'] = 'String10 in Lexicon file';
+$_lang['string12'] = 'String12 in Lexicon file';
