7d18ab02294620d0721b064aa9c2aa8728087db8
bobray <bobray@softville.com>
2012-11-21 22:18:25 -0600

    plugin 2

--- core/components/example/elements/plugins/plugin2.plugin.php
+++ core/components/example/elements/plugins/plugin2.plugin.php
@@ -1,8 +1,10 @@
+<?php
+
 /**
  * Plugin2 plugin for Example extra
  *
  * Copyright 2012 by Bob Ray <http://bobsguides.com>
- * Created on 10-31-2012
+ * Created on 08-21-2012
  *
  * Example is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -32,3 +34,41 @@
  *
  * @package example
  **/
+
+$strings = array(
+    "\$this->modx->getService('registry', 'Example')",
+    "\$this->modx->getService('login', 'Example2')",
+    "\$modx->loadClass('login.Example2'",
+    "\$modx->loadClass('login.hello.Example3'",
+);
+foreach($strings as $string) {
+    if (strstr($string,'modx->getService')) {
+        $pattern  =  "/modx\s*->\s*getService\s*\(\s*\'[^,]*,\s*'([^']*)/";
+        preg_match($pattern, $string, $matches);
+        $s = strtoLower($matches[1]);
+        if(strstr($s, '.')) {
+            $r = strrev($s);
+            $class = strrev(substr($r,0, strpos($r, '.')));
+        } else {
+            $class = $s;
+        }
+        echo "\n" . $class;
+    }
+    if (strstr($string, 'modx->loadClass')) {
+        $pattern = "/modx\s*->\s*loadClass\s*\(\s*\'([^']*)/";
+        preg_match($pattern, $string, $matches);
+        $s = strtoLower($matches[1]);
+        if (strstr($s, '.')) {
+            $r = strrev($s);
+            $class = strrev(substr($r, 0, strpos($r, '.')));
+        }
+        else {
+            $class = $s;
+        }
+        echo "\n" . $class;
+    }
+    $x = $modx->getOption($variable, $this->props, null);
+    $x = $modx->getOption('variable1', $this->props, null);
+    $x = getProperty('variable2', null);
+    $x = $scriptProperties['pulled_with_scriptProperties'];
+}
